c2dd5f68cea02dad8c7e04a2b49021bc7ab2ce27
Chebroluhemanthgithub <hemanthchebrolu21@gmail.com>
2025-11-24 15:50:02 +0530

    Updated assignment code

--- src/data/Users.js
+++ src/data/Users.js
@@ -1,5 +1,5 @@
 export const users = [
   { id: 1, username: "Manager", password: "PM123", role: "PM" },
-  { id: 2, username: "user1", password: "123", role: "User" },
-  { id: 3, username: "user2", password: "123", role: "User" }
+  { id: 2, username: "User1", password: "123", role: "User" },
+  { id: 3, username: "User2", password: "123", role: "User" }
 ];
